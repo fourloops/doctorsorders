77a2fdf4bac268e7aa2aafc5533caa4a9085c871
Owen TM <oturnermajor@gmail.com>
2016-01-18 19:41:19 +0000

    more edits to javascript

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+document.getElementById("submitButton").addEventListener("click", radios);
 
-var listen = document.getElementById("submitButton");
-listen.addEventListener("click", radios);
+var totalScore = 0;
 
+// Cycling through radio buttons and adding  value to totalScore if checked.
 function radios(){
-    var totalScore = 0;
     for (var i = 1; i<13; i++){
         var string = 'r'+i;
         var radioB = document.getElementById(string);
@@ -11,19 +11,16 @@ function radios(){
             totalScore += +radioB.value;
         }
     }
-    console.log(totalScore, 'clicked');
+    // Converting totalScore to percentage (rounded and then turned back to integer)
+    percentScore = +((totalScore/9)*100).toFixed();
+    // Writing text output to result div
+    if (totalScore > 6){
+        document.getElementById('results').innerHTML += 'Pat on the back for you! Your score is '+percentScore+'%!';
+    } else if (totalScore > 3) {
+        document.getElementById('results').innerHTML += 'Almost there! Your score is '+percentScore+'%!';
+    } else if (totalScore > 0) {
+        document.getElementById('results').innerHTML += 'Go to the gym! Your score is '+percentScore+'%!';
+    } else {
+        document.getElementById('results').innerHTML += 'Call an ambulance! Your score is '+percentScore+'%!';
+    }
 }
-
-
-
-// totalScore = totalScore*10;
-
-//if (totalScore > 6){
-//    document.write('Pat on the back for you! Your score is '+totalScore);
-//} else if (totalScore > 3) {
-//    document.write('Almost there! Your score is '+totalScore);
-//} else if (totalScore > 0) {
-//    document.write('Go to the gym Your score is '+totalScore);
-//} else {
-//    document.write('Call an ambulance Your score is '+totalScore);
-//}
